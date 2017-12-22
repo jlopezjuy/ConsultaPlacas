7a9413db47d7582a0e2519713e838af00f37f74b
Unknown <jlopezjuy@gmail.com>
2017-12-22 15:29:35 -0300

    CAmbios en Radio

--- src/main/java/org/seguritech/cp/repository/RadioRepository.java
+++ src/main/java/org/seguritech/cp/repository/RadioRepository.java
@@ -1,9 +1,6 @@
 package org.seguritech.cp.repository;
 
 import org.seguritech.cp.domain.Radio;
-import org.seguritech.cp.service.dto.RadioDTO;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
